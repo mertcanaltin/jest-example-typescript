6273ea5a9eae4352bcab4fcab6853da266b01296
Mert Can Altın <mertgold60@gmail.com>
2022-08-25 19:11:04 +0300

    added test comment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// We outline the objectives of the test.:
 test('renders learn react link', () => {
+  // Arrange: We get ready the testing environment, and rendering of the component;
   render(<App />);
+
+  // Act: Look for the anticipated link.
   const linkElement = screen.getByText(/learn react/i);
+
+  // Assert: check that the document contains the necessary link.
   expect(linkElement).toBeInTheDocument();
 });
